a649b5bfcc5498d551b41f12f6bc3c8cc93adaa3
metmets <markuserik10@gmail.com>
2024-04-15 15:05:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,25 @@ import asyncio
 import math
 from pymavlink import mavutil
 
-"""
-master = mavutil.mavlink_connection('com14', baud=57600)
+class message:
+    def __init__(self):
+        self.lat = 0
+        self.lon = 0
+        self.alt = 0
+        self.message = "test"
+    
+    def getlonlat(self, message):
+        print(f"lon: {message.lon}, lat: {message.lat}, alt: {message.alt}, relalt: {message.relative_alt}")
+        self.lon = int(message.lon)/10000000
+        self.lat = int(message.lat)/10000000
+    
+message = message()
+
+master = mavutil.mavlink_connection('udpin:127.0.0.1:14551')
 master.wait_heartbeat()
 master.mav.param_request_list_send(
     master.target_system, master.target_component
 )
-"""
 
 def getlonlat(message):
     print(f"lon: {message.lon}, lat: {message.lat}, alt: {message.alt}, relalt: {message.relative_alt}")
@@ -25,6 +37,7 @@ def distance(glon, glat, dlon, dlat):
     R = 6372.795477598
     dist = R*math.acos(math.sin(glat)*math.sin(dlat)+math.cos(glat)*math.cos(dlat)*math.cos(glon-dlon))
     print(dist)
+    return(dist)
 
 def direction(glon, glat, dlon, dlat):
     # Calculate difference in longitude, considering the shortest angular distance
@@ -38,8 +51,16 @@ def direction(glon, glat, dlon, dlat):
     # Calculate bearing
     deltafi = math.log((math.tan((dlat*0.5) + (math.pi*0.25))) / (math.tan(glat*0.5 + math.pi*0.25)))
     bear = math.atan2(deltalon, deltafi)
-    print(bear)
+    bear_deg = bear * 180/math.pi
+    print(bear_deg)
 
+def verticaldirection(glon, glat, dlon, dlat, alt):
+    h = 3
+    dist = distance(glon, glat, dlon, dlat)
+    alt = alt - dist*0.08
+    angle = math.atan((int(alt)-h)/dist)
+    angle_deg = angle * 180/math.pi
+    print(angle_deg)
 
 def datafromtelemetry():
     while True:
@@ -62,5 +83,9 @@ def ll(coord):
     coord = coord*math.pi/180
     return(coord)
 
+datafromtelemetry()
 direction(ll(266901429),ll(583103823),ll(242818695),ll(594573600))
+distance(ll(266901429),ll(583103823),ll(242818695),ll(594573600))
+verticaldirection(ll(266901429),ll(583103823),ll(242818695),ll(594573600), 10)
+
 #lat : 583103823, lon : 266901429
